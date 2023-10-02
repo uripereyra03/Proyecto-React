592ee9c9fbeac119583bbf4ccb4e294583bed292
Uriel Pereyra <urielinstituto@gmail.com>
2023-10-02 12:32:20 -0300

    Correcciones

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import NavBar from './components/NavBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemListContainer from './components/ItemListConteiner';
 import ItemDetailContainer from "./components/itemdetailconteiner"
-import { getProducts as fetchProducts } from './components/asyncMock';
 
 const MyApp = () => {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await fetchProducts();
-                setProducts(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
     return (
         <BrowserRouter>
             <NavBar />
             <Routes>
-                <Route path="/" element={<ItemListContainer products={products} fetchProducts={fetchProducts} />} />
-                <Route path="/category/:categoryId" element={<ItemListContainer products={products} fetchProducts={fetchProducts} />} />
+                <Route path="/" element= {<ItemListContainer/>}/>
+                <Route path="/category/:categoryId" element={<ItemListContainer/>} />
                 <Route path="/item/:itemId" element={<ItemDetailContainer />} />
             </Routes>
         </BrowserRouter>
--- src/components/ItemListConteiner.js
+++ src/components/ItemListConteiner.js
@@ -1,43 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Item from './item';
+import Itemlist from "./itemlist";
+import {fetchProducts, getProductsByCategory} from "./asyncMock";
 
-const ItemList = ({ products }) => {
-    products = products || []; 
-    console.log('Estado products en ItemList:', products);
-    return (
-        <div className="row">
-            {products.map((producto) => (
-                <Item key={producto.id} producto={producto} />
-            ))}
-        </div>
-    );
-};
-
-
-const ItemListContainer = ({ fetchProducts }) => {
+const ItemListContainer = () => {
     const { categoryId } = useParams();
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await fetchProducts();
-                setProducts(data);
-
-                console.log('Estado products en ItemListContainer:', data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchData();
+        if (categoryId) {
+            getProductsByCategory(categoryId) .then((el) =>setProducts(el));
+        } else {
+            fetchProducts().then((el) => setProducts(el)); 
+        }
     }, [categoryId]);
-
+    console.log (products);
     return (
         <div>
             <h2>Calzado</h2>
-            <ItemList products={products} />
+            <Itemlist products={products} />
             <p>Esta es mi tienda en línea</p>
         </div>
     );
